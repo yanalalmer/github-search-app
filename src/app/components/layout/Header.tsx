cda9e530fa62c0a1d873e1eaca5c4a379f94a22a
yanal-almir <149601353+yanal-almir@users.noreply.github.com>
2024-09-16 13:53:01 +0200

    added global styles and tailwind theme config

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className='flex justify-between items-center'>
+    <header className='flex justify-between items-center bg-background-btn w-full'>
       <h1 className='text-2xl font-bold text-text-primary'>GithubFinder</h1>
       <p>toggle btn</p>
     </header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: 'var(--background)',
+        'background-secondary': 'var(--background-secondary)',
+        'background-btn': 'var(--background-btn)',
+        'text-primary': 'var(--text-primary)',
+        'text-secondary': 'var(--text-secondary)',
+        'box-shadow': 'var(--box-shadow)',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
