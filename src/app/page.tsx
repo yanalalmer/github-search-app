41d26e11c19cee518157dc7f5a7389562f650d3e
yanal-almir <149601353+yanal-almir@users.noreply.github.com>
2024-09-16 13:53:52 +0200

    added page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from '@/components/layout/Header';
 
 export default function Home() {
   return (
-    <main>
+    <main className='m-auto max-w-3xl w-full'>
       <Header />
     </main>
   );
